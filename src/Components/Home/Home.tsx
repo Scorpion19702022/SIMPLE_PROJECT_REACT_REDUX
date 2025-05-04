a95f32473132fd9cb38772855ca9632651ad523a
skorpion <jmosinski@o2.pl>
2025-05-04 20:28:41 +0200

    added styles

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -3,7 +3,7 @@ import styles from './Styles/Home.module.css'
 const Home = () => {
 	return (
 		<div className={styles.wrapper}>
-			<h1>HOME</h1>
+			<h1 className={styles.heading}>HOME</h1>
 		</div>
 	)
 }
