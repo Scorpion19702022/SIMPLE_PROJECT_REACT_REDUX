e932db981065f09111b4d087003363fbd310efd7
skorpion <jmosinski@o2.pl>
2025-05-05 20:09:18 +0200

    added style

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,18 @@
 import styles from './Styles/Home.module.css'
 
+import code01 from '../../assets/code01.png'
+
 const Home = () => {
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.card_front}></div>
-			<div className={styles.card_back}></div>
+			<div className={styles.box_card}>
+				<div className={styles.card_front}>
+					<img className={styles.img_front} src={code01} alt='picture_code' />
+					<div className={styles.shadow}></div>
+					<button>odwróć kartę</button>
+				</div>
+				<div className={styles.card_back}></div>
+			</div>
 		</div>
 	)
 }
