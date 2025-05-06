60289e23e028225148ed57ce3a9a336ab332b8a4
skorpion <jmosinski@o2.pl>
2025-05-06 21:01:26 +0200

    added function

--- src/Components/Home/HomeComponent.tsx
+++ src/Components/Home/HomeComponent.tsx
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 import Home from './Home'
 import styles from './Styles/HomeComponent.module.css'
 
 const HomeComponent = () => {
+	const [action, setAction] = useState<boolean>(false)
+
+	const handleActionCard = () => {
+		setAction(!action)
+	}
+
 	return (
 		<section className={styles.wrapper}>
-			<Home />
+			<Home actionCard={action} addActionCard={handleActionCard} />
 		</section>
 	)
 }
