76bcdb3e672a533bda4cae78bd72b7759dcac757
skorpion <jmosinski@o2.pl>
2025-05-06 19:43:38 +0200

    added style

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -8,9 +8,9 @@ const Home = () => {
 		<div className={styles.wrapper}>
 			<div className={styles.box_card}>
 				<div className={styles.card_front}>
-					<h2 className={styles.heading_front}>Projekty w REDUX</h2>
 					<img className={styles.img_front} src={code01} alt='picture_code' />
 					<div className={styles.shadow_front}></div>
+					<h2 className={styles.heading_front}>Projekty w REDUX</h2>
 					<button className={styles.btn_front}>odwróć kartę</button>
 				</div>
 				<div className={styles.card_back}>
