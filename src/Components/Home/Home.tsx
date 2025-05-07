d99117a99a210cfc7c8baad22ba33f9699ab3b73
skorpion <jmosinski@o2.pl>
2025-05-07 19:51:56 +0200

    added text and style

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -26,10 +26,7 @@ const Home: React.FC<InitialProps> = ({ actionCard, addActionCard }) => {
 					<h2 className={styles.heading_back}>Opis projektów</h2>
 					<img className={styles.img_back} src={code02} alt='picture_code' />
 					<div className={styles.shadow_back}></div>
-					<p className={styles.text_back}>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nihil recusandae necessitatibus quibusdam
-						quasi blanditiis explicabo quam facilis repellendus labore.
-					</p>
+					<p className={styles.text_back}>Projekty w React i TypeScript z wykorzystaniem Redux.</p>
 					<button className={styles.btn_back} onClick={addActionCard}>
 						odwróć kartę
 					</button>
