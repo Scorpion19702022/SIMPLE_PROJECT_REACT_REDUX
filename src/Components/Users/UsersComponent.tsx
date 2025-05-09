c25eb91282a46437cf19a33f7ef6fe851a89b363
skorpion <jmosinski@o2.pl>
2025-05-09 18:03:10 +0200

    added function and state

--- src/Components/Users/UsersComponent.tsx
+++ src/Components/Users/UsersComponent.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react'
 import styles from './Styles/UsersComponent.module.css'
 import UsersInputs from './UsersInputs'
 
 const UsersComponent = () => {
+	const [firstName, setFirstName] = useState<string>('')
+	const [lastName, setLastName] = useState<string>('')
+	const [gender, setGender] = useState<string>('')
+
+	const handleChangeFirstName = (e: string) => {}
+
+	const handleChangeLastName = (e: string) => {}
+
+	const handleChangeGender = (e: string) => {}
+
 	return (
 		<section className={styles.wrapper}>
 			<UsersInputs />
