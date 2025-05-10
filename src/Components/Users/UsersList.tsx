05cddf13e67cfe5f33a7c9dbab3be8fa19324a8f
skorpion <jmosinski@o2.pl>
2025-05-10 18:12:11 +0200

    added style for mobile

--- src/Components/Users/UsersList.tsx
+++ src/Components/Users/UsersList.tsx
@@ -3,22 +3,22 @@ import styles from './Styles/UsersList.module.css'
 const UsersList = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h3 className={styles.heding}>Lista użytkowików</h3>
+			<h3 className={styles.heading}>Lista użytkowików</h3>
 			<div className={styles.box_count}>
-				<span className={styles.spa_count}>
+				<span className={styles.span_count}>
 					wszyscy: <span className={styles.count}>0</span>
 				</span>
-				<span className={styles.spa_count}>
+				<span className={styles.span_count}>
 					koiety: <span className={styles.count}>0</span>
 				</span>
-				<span className={styles.spa_count}>
+				<span className={styles.span_count}>
 					mężczyźni: <span className={styles.count}>0</span>
 				</span>
 			</div>
 			<div className={styles.box_btn_select}>
-				<button className={styles.btn_select}></button>
-				<button className={styles.btn_select}></button>
-				<button className={styles.btn_select}></button>
+				<button className={styles.btn_select}>wszyscy</button>
+				<button className={styles.btn_select}>kobiety</button>
+				<button className={styles.btn_select}>mężczyźni</button>
 			</div>
 			<div className={styles.box_list_users}>
 				<p>lista</p>
