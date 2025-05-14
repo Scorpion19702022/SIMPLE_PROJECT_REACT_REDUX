c2f1c175d52c696e99ae6c691fc0077ae140bc2c
skorpion <jmosinski@o2.pl>
2025-05-14 19:38:12 +0200

    added props

--- src/Components/Notes/NotesComponent.tsx
+++ src/Components/Notes/NotesComponent.tsx
@@ -5,11 +5,14 @@ import styles from './Styles/NotesComponent.module.css'
 const NotesComponent = () => {
 	const currentDate = new Date().toLocaleString('en-CA').slice(0, 10)
 
+	const [theme, setTheme] = useState<string>('')
 	const [inputDate, setInputDate] = useState<string>(currentDate)
+	const [text, setText] = useState<string>('')
+	const [kind, setKind] = useState<string>('wybierz kategorię')
 
 	return (
 		<section className={styles.wrapper}>
-			<NotesInputs date={inputDate} />
+			<NotesInputs inputTheme={theme} date={inputDate} inputText={text} inputSelect={kind} />
 		</section>
 	)
 }
