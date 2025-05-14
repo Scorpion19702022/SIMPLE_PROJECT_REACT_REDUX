f1d9f7a4a195b3ffcabe0a956f958917a51d926e
skorpion <jmosinski@o2.pl>
2025-05-14 17:18:48 +0200

    added form for NotesInputs

--- src/Components/Notes/NotesInputs.tsx
+++ src/Components/Notes/NotesInputs.tsx
@@ -4,6 +4,32 @@ const Notes = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h2 className={styles.heading}>Wypełnij formularz</h2>
+			<form className={styles.form}>
+				<div className={styles.box_inputs}>
+					<label className={styles.label}>wpisz tytuł:</label>
+					<input className={styles.input} type='text' />
+					<span className={styles.error}>error</span>
+				</div>
+				<div className={styles.box_inputs}>
+					<label className={styles.label}>wybierz datę:</label>
+					<input className={styles.input_date} type='date' />
+					<span className={styles.error}>error</span>
+				</div>
+				<div className={styles.box_inputs}>
+					<label className={styles.label}>wpisz tekst:</label>
+					<span className={styles.marks}>Znaków: 0</span>
+					<textarea className={styles.textarea}></textarea>
+					<span className={styles.error}>error</span>
+				</div>
+				<div className={styles.box_inputs}>
+					<label className={styles.label}>wybierz rodzaj</label>
+					<select className={styles.select}></select>
+					<span className={styles.error}>error</span>
+				</div>
+				<button className={styles.btn} type='submit'>
+					dodaj
+				</button>
+			</form>
 		</section>
 	)
 }
