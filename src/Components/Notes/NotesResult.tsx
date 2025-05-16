720078fa509a87c54261fa7f872f4baf46d361ff
skorpion <jmosinski@o2.pl>
2025-05-16 19:30:04 +0200

    added color

--- src/Components/Notes/NotesResult.tsx
+++ src/Components/Notes/NotesResult.tsx
@@ -4,7 +4,12 @@ const NotesResult = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h3 className={styles.heading}>Notes</h3>
-			<div className={styles.box_notes}></div>
+			<div className={styles.box_notes}>
+				<div className={styles.box01}></div>
+				<div className={styles.box02}></div>
+				<div className={styles.box03}></div>
+				<div className={styles.box04}></div>
+			</div>
 		</section>
 	)
 }
