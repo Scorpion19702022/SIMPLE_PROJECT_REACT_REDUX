b8df5ff18fd034c441fa7f82c36c652bb6b60ac9
skorpion <jmosinski@o2.pl>
2025-05-17 17:38:34 +0200

    added content for component NotesResult

--- src/Components/Notes/NotesResult.tsx
+++ src/Components/Notes/NotesResult.tsx
@@ -4,12 +4,22 @@ const NotesResult = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h3 className={styles.heading}>Notes</h3>
-			<div className={styles.box_notes}>
-				<div className={styles.box01}></div>
-				<div className={styles.box02}></div>
-				<div className={styles.box03}></div>
-				<div className={styles.box04}></div>
+			<div className={styles.box_info_select}>
+				<div className={styles.box_counter}>
+					<span className={styles.counter}>
+						Ilość notatek: <span className={styles.count}>0</span>
+					</span>
+				</div>
+				<div className={styles.box_btns_select}>
+					<button className={styles.btn_select}>wszystkie</button>
+					<button className={styles.btn_select}>dom</button>
+					<button className={styles.btn_select}>praca</button>
+					<button className={styles.btn_select}>rozrywka</button>
+					<button className={styles.btn_select}>inne</button>
+				</div>
+				<button className={styles.btn_clean_all}>usuń wszystko</button>
 			</div>
+			<div className={styles.box_notes}></div>
 		</section>
 	)
 }
