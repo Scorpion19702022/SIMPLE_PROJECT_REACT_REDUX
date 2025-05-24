65fa31dd40dfc8cca9b20b4b0d1e2fe218bea983
skorpion <jmosinski@o2.pl>
2025-05-24 17:56:59 +0200

    install react-icons

--- src/Components/Notes/NotesItem.tsx
+++ src/Components/Notes/NotesItem.tsx
@@ -1,12 +1,16 @@
 import styles from './Styles/NotesItem.module.css'
 
+import { RiDeleteBin6Line } from 'react-icons/ri'
+
 const NotesItem = () => {
 	return (
 		<div className={styles.card}>
 			<h3 className={styles.theme}></h3>
 			<p className={styles.text}></p>
 			<em className={styles.date}></em>
-			<button className={styles.btn}></button>
+			<button className={styles.btn}>
+				<RiDeleteBin6Line />
+			</button>
 		</div>
 	)
 }
