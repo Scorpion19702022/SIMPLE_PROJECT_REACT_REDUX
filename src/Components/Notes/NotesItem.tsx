210a983ffd14552e4000f97b9809f1efc8f36303
skorpion <jmosinski@o2.pl>
2025-05-25 20:25:51 +0200

    added style

--- src/Components/Notes/NotesItem.tsx
+++ src/Components/Notes/NotesItem.tsx
@@ -6,14 +6,18 @@ const NotesItem = () => {
 	return (
 		<div className={styles.card}>
 			<h3 className={styles.theme}>Tytuł</h3>
-			<p className={styles.text}>
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. In minima nostrum ipsam repellat sunt optio tempora a
-				fugit nisi libero.
-			</p>
-			<em className={styles.date}>12.11.1970</em>
-			<button className={styles.btn}>
-				<RiDeleteBin6Line className={styles.bin} />
-			</button>
+			<div className={styles.box_content}>
+				<p className={styles.text}>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. In minima nostrum ipsam repellat sunt optio tempora
+					a fugit nisi libero.
+				</p>
+				<em className={styles.date}>12.11.1970</em>
+			</div>
+			<div className={styles.box_btn}>
+				<button className={styles.btn}>
+					<RiDeleteBin6Line className={styles.bin} />
+				</button>
+			</div>
 		</div>
 	)
 }
