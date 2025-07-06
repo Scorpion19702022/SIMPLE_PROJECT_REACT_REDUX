16fdead8ce1057935d12621e3842a140f6159635
skorpion <jmosinski@o2.pl>
2025-07-06 16:09:27 +0200

    added style

--- src/Components/Magic8Ball/Magic8BallAction.tsx
+++ src/Components/Magic8Ball/Magic8BallAction.tsx
@@ -3,8 +3,8 @@ import styles from './Styles/Magic8BallAction.module.css'
 const Magic8BallAction = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h1>Magic8Ball</h1>
-			<span>wpisz pytanie,kliknij w bilę i poznaj odpowiedź</span>
+			<h1 className={styles.heading}>Magic8Ball</h1>
+			<span className={styles.text}>wpisz pytanie,kliknij w bilę i poznaj odpowiedź</span>
 			<div></div>
 		</section>
 	)
