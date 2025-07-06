7701f3b41082a9c8b4cec7e19939aa33192323b5
skorpion <jmosinski@o2.pl>
2025-07-06 16:02:53 +0200

    added component

--- src/Components/Magic8Ball/Magic8Ball.tsx
+++ src/Components/Magic8Ball/Magic8Ball.tsx
@@ -1,9 +1,10 @@
+import Magic8BallAction from './Magic8BallAction'
 import styles from './Styles/Magic8Ball.module.css'
 
 const Magic8Ball = () => {
 	return (
 		<main className={styles.wrapper}>
-			<h1>MagicBall</h1>
+			<Magic8BallAction />
 		</main>
 	)
 }
---
+++ src/Components/Magic8Ball/Magic8BallAction.tsx
@@ -0,0 +1,13 @@
+import styles from './Styles/Magic8BallAction.module.css'
+
+const Magic8BallAction = () => {
+	return (
+		<section className={styles.wrapper}>
+			<h1>Magic8Ball</h1>
+			<span>wpisz pytanie,kliknij w bilę i poznaj odpowiedź</span>
+			<div></div>
+		</section>
+	)
+}
+
+export default Magic8BallAction
