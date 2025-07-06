39a44e18f9c893ccfc295c5214ccb6437a679f69
skorpion <jmosinski@o2.pl>
2025-07-06 16:45:21 +0200

    added image and style

--- src/Components/Magic8Ball/Magic8BallAction.tsx
+++ src/Components/Magic8Ball/Magic8BallAction.tsx
@@ -1,11 +1,17 @@
 import styles from './Styles/Magic8BallAction.module.css'
 
+import imgBall from '../../assets/ball-35516_1920.png'
+
 const Magic8BallAction = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h1 className={styles.heading}>Magic8Ball</h1>
 			<span className={styles.text}>wpisz pytanie,kliknij w bilę i poznaj odpowiedź</span>
-			<div></div>
+			<div className={styles.box_img}>
+				<button className={styles.btn_ball}>
+					<img className={styles.img} src={imgBall} alt='ball' />
+				</button>
+			</div>
 		</section>
 	)
 }
