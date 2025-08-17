ef63f20ed4de31ca56f2edb686278a3d6816accb
skorpion <jmosinski@o2.pl>
2025-08-17 19:08:38 +0200

    added new project

---
+++ src/Components/Wallet/Wallet.tsx
@@ -0,0 +1,11 @@
+import styles from './Styles/Wallet.module.css'
+
+const Wallet = () => {
+	return (
+		<main className={styles.wrapper}>
+			<h1>Wallet</h1>
+		</main>
+	)
+}
+
+export default Wallet
