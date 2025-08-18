a297bb8520c6cf3f222d9b5405949ba4274486b5
skorpion <jmosinski@o2.pl>
2025-08-18 20:50:09 +0200

    added component

--- src/Components/Wallet/WalletList.tsx
+++ src/Components/Wallet/WalletList.tsx
@@ -4,7 +4,14 @@ const WalletList = () => {
 	return (
 		<section className={styles.wrapper}>
 			<h2 className={styles.heading}>{`lista transakcji`.toLocaleUpperCase()}</h2>
-			<div className={styles.box_deal}></div>
+			<div className={styles.box_deal}>
+				<div className={styles.box_income}>
+					<h3 className={styles.heading_income}>{`przychody`.toLocaleUpperCase()}</h3>
+				</div>
+				<div className={styles.box_expense}>
+					<h3 className={styles.heading_expense}>{`wydatki`.toLocaleUpperCase()}</h3>
+				</div>
+			</div>
 		</section>
 	)
 }
