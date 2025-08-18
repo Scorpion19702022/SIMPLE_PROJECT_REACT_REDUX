96871b03e0a072b7f2ade775c2c3f2dfeaa5e357
skorpion <jmosinski@o2.pl>
2025-08-18 20:02:35 +0200

    added content in component

--- src/Components/Wallet/WalletManager.tsx
+++ src/Components/Wallet/WalletManager.tsx
@@ -3,7 +3,12 @@ import styles from './Styles/WalletManager.module.css'
 const WalletManager = () => {
 	return (
 		<section className={styles.wrapper}>
-			<h2 className={styles.heading}>{`dostępne środki`.toLocaleUpperCase()}</h2>
+			<div className={styles.header}>
+				<h2 className={styles.heading}>{`dostępne środki:`.toLocaleUpperCase()}</h2>
+				<span className={styles.count}>
+					0<span className={styles.span_count}> zł</span>
+				</span>
+			</div>
 		</section>
 	)
 }
