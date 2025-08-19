7b6107f042092d22a878cd7c0192ad11a98fcfa8
skorpion <jmosinski@o2.pl>
2025-08-19 20:54:00 +0200

    added style

--- src/Components/Wallet/WalletList.tsx
+++ src/Components/Wallet/WalletList.tsx
@@ -7,11 +7,41 @@ const WalletList = () => {
 			<div className={styles.box_deal}>
 				<div className={styles.box_income}>
 					<h3 className={styles.heading_income}>{`przychody`.toLocaleUpperCase()}</h3>
-					<div className={styles.income_list}>LISTA</div>
+					<div className={styles.deal_list}>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+					</div>
 				</div>
 				<div className={styles.box_expense}>
 					<h3 className={styles.heading_expense}>{`wydatki`.toLocaleUpperCase()}</h3>
-					<div className={styles.expense_list}>LISTA</div>
+					<div className={styles.deal_list}>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+						<p className={styles.list}>lista</p>
+					</div>
 				</div>
 			</div>
 		</section>
