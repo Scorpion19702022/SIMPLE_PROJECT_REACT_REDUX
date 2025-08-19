d896c521708625c2dfa315d5fbf2d9340c59e310
skorpion <jmosinski@o2.pl>
2025-08-19 20:34:22 +0200

    added content for component

--- src/Components/Wallet/WalletList.tsx
+++ src/Components/Wallet/WalletList.tsx
@@ -7,9 +7,11 @@ const WalletList = () => {
 			<div className={styles.box_deal}>
 				<div className={styles.box_income}>
 					<h3 className={styles.heading_income}>{`przychody`.toLocaleUpperCase()}</h3>
+					<div className={styles.income_list}>LISTA</div>
 				</div>
 				<div className={styles.box_expense}>
 					<h3 className={styles.heading_expense}>{`wydatki`.toLocaleUpperCase()}</h3>
+					<div className={styles.expense_list}>LISTA</div>
 				</div>
 			</div>
 		</section>
