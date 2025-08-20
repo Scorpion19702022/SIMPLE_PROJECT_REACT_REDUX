2b005d5254e4148559daccd8251b919292b0ccd5
skorpion <jmosinski@o2.pl>
2025-08-20 19:47:22 +0200

    import components to WalletList

--- src/Components/Wallet/WalletList.tsx
+++ src/Components/Wallet/WalletList.tsx
@@ -1,4 +1,6 @@
 import styles from './Styles/WalletList.module.css'
+import WalletExpenseItem from './WalletExpenseItem'
+import WalletIncomeItem from './WalletIncomeItem'
 
 const WalletList = () => {
 	return (
@@ -7,11 +9,39 @@ const WalletList = () => {
 			<div className={styles.box_deal}>
 				<div className={styles.box_income}>
 					<h3 className={styles.heading_income}>{`przychody`.toLocaleUpperCase()}</h3>
-					<div className={styles.deal_list}></div>
+					<div className={styles.deal_list}>
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+						<WalletIncomeItem />
+					</div>
 				</div>
 				<div className={styles.box_expense}>
 					<h3 className={styles.heading_expense}>{`wydatki`.toLocaleUpperCase()}</h3>
-					<div className={styles.deal_list}></div>
+					<div className={styles.deal_list}>
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+						<WalletExpenseItem />
+					</div>
 				</div>
 			</div>
 		</section>
