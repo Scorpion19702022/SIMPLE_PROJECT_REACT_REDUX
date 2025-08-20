0fff2beb8089e8b74cec77d43da8983f1c6f7989
skorpion <jmosinski@o2.pl>
2025-08-20 19:38:52 +0200

    added components

---
+++ src/Components/Wallet/WalletIncomeItem.tsx
@@ -0,0 +1,10 @@
+const WalletIncomeItem = () => {
+	return (
+		<div>
+			<span></span>
+			<span></span>
+		</div>
+	)
+}
+
+export default WalletIncomeItem
--- src/Components/Wallet/WalletList.tsx
+++ src/Components/Wallet/WalletList.tsx
@@ -7,41 +7,11 @@ const WalletList = () => {
 			<div className={styles.box_deal}>
 				<div className={styles.box_income}>
 					<h3 className={styles.heading_income}>{`przychody`.toLocaleUpperCase()}</h3>
-					<div className={styles.deal_list}>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-					</div>
+					<div className={styles.deal_list}></div>
 				</div>
 				<div className={styles.box_expense}>
 					<h3 className={styles.heading_expense}>{`wydatki`.toLocaleUpperCase()}</h3>
-					<div className={styles.deal_list}>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-						<p className={styles.list}>lista</p>
-					</div>
+					<div className={styles.deal_list}></div>
 				</div>
 			</div>
 		</section>
