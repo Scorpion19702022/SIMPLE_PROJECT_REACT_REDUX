143c77af7bc0ee1544386dda897e25f4fdea1f4f
skorpion <jmosinski@o2.pl>
2025-08-20 20:06:32 +0200

    added component

---
+++ src/Components/Wallet/WalletInputsModal.tsx
@@ -0,0 +1,13 @@
+import styles from './Styles/WalletInputsModal.module.css'
+
+const WalletInputsModal = () => {
+	return (
+		<section className={styles.wrapper}>
+			<form>
+				<div></div>
+			</form>
+		</section>
+	)
+}
+
+export default WalletInputsModal
