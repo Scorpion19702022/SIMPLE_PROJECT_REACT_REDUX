926bd4dd0b3654c61247bb5bc353bd1cabcc4e31
skorpion <jmosinski@o2.pl>
2025-08-20 19:41:51 +0200

    added content for component wallet item income

--- src/Components/Wallet/WalletIncomeItem.tsx
+++ src/Components/Wallet/WalletIncomeItem.tsx
@@ -1,8 +1,10 @@
+import styles from './Styles/WalletIncomeItem.module.css'
+
 const WalletIncomeItem = () => {
 	return (
-		<div>
-			<span></span>
-			<span></span>
+		<div className={styles.box_income}>
+			<span className={styles.income_theme}>Temat</span>
+			<span className={styles.income_worth}>5000</span>
 		</div>
 	)
 }
