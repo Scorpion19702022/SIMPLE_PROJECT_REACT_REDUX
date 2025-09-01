543635233bb19bf88eecd84b296eab74f162805e
skorpion <jmosinski@o2.pl>
2025-09-01 18:40:16 +0200

    added function

--- src/Components/Wallet/Wallet.tsx
+++ src/Components/Wallet/Wallet.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
 import styles from './Styles/Wallet.module.css'
 import WalletInputsModal from './WalletInputsModal'
 import WalletList from './WalletList'
 import WalletManager from './WalletManager'
 
 const Wallet = () => {
+	const [activeModal, setActiveModal] = useState<boolean>(false)
+
+	const handleActiveModal = () => {
+		setActiveModal(true)
+	}
+
+	const handleCacelModal = () => {
+		setActiveModal(false)
+	}
+
 	return (
 		<main className={styles.wrapper}>
 			<div className={styles.header}>
@@ -11,8 +22,8 @@ const Wallet = () => {
 				<span className={styles.text_heading}>- twój portfel -</span>
 			</div>
 			<div className={styles.box_wallet}>
-				<WalletInputsModal />
-				<WalletManager />
+				<WalletInputsModal isCancelModal={activeModal} cancelModal={handleCacelModal} />
+				<WalletManager activeModal={handleActiveModal} />
 				<WalletList />
 			</div>
 		</main>
