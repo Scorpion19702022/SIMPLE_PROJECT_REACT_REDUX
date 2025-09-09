54da1fcc1c1dcd0011a6170afe121055e4fcb97a
skorpion <jmosinski@o2.pl>
2025-09-09 20:52:13 +0200

    added calculate total sum for component

--- src/Components/Wallet/WalletManager.tsx
+++ src/Components/Wallet/WalletManager.tsx
@@ -1,3 +1,5 @@
+import { selectTotalSum } from '../../Features/Wallet/WalletSelectors'
+import { useAppSelector } from '../../hooks/useReduxHooks'
 import styles from './Styles/WalletManager.module.css'
 
 interface initialProps {
@@ -6,12 +8,15 @@ interface initialProps {
 }
 
 const WalletManager: React.FC<initialProps> = ({ activeModal, deleteAllDeal }) => {
+	const totalSum = useAppSelector(selectTotalSum)
+
 	return (
 		<section className={styles.wrapper}>
 			<div className={styles.header}>
 				<h2 className={styles.heading}>{`dostępne środki:`.toLocaleUpperCase()}</h2>
 				<span className={styles.count}>
-					0<span className={styles.span_count}> zł</span>
+					{totalSum}
+					<span className={styles.span_count}> zł</span>
 				</span>
 			</div>
 			<div className={styles.box_btns}>
