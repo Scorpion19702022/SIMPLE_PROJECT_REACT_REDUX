b814c68d08d8b73bff3b2d64625971cc02c0d0e7
skorpion <jmosinski@o2.pl>
2025-09-10 21:27:37 +0200

    added localStorage from store

--- src/Features/store.ts
+++ src/Features/store.ts
@@ -20,8 +20,8 @@ store.subscribe(() => {
 		localStorage.removeItem('walletIncome')
 		localStorage.removeItem('walletExpense')
 	} else {
-		localStorage.getItem('walletIncome', JSON.stringify(state.walletIncome))
-		localStorage.getItem('walletExpense', JSON.stringify(state.walletExpense))
+		localStorage.setItem('walletIncome', JSON.stringify(state.walletIncome))
+		localStorage.setItem('walletExpense', JSON.stringify(state.walletExpense))
 	}
 })
 
